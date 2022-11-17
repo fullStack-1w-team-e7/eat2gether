7342ef7a5b7bd00cfbb2ef6cf6a7792dedabade8
yena choi <geniusyena332@gmail.com>
2022-11-17 17:08:35 +0900

    기능 합치기 성공 / register 에서 회원아이디 중복 및 글자수 체크하는 로직 추가 전

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ SECRET_KEY = config['SECRET_KEY']
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
+    print(token_receive)
+
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        print(payload['id'])
         user_info = db.info.find_one({"id": payload['id']})
         return render_template('main.html', user_id=user_info["id"])
     except jwt.ExpiredSignatureError:
@@ -61,33 +62,27 @@ def post_info(post_id):
 
 # [로그인 API]
 # id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
-@app.route('/api/login', methods=['POST'])
+@app.route("/api/login", methods=["POST"])
 def api_login():
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
-    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    # 비밀번호를 암호화
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
     result = db.info.find_one({'id': id_receive, 'pw': pw_hash})
 
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
-    # (!access tocken, refresh tocken 찾아보기)
     if result is not None:
-        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
-        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
-        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
-        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
         }
+        # 로그인이 얼마나 유지되는가
+
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-        # token을 줍니다.
         return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
@@ -276,14 +271,6 @@ def delete_post(post_id):
         return jsonify({'result': 'success', 'msg': '삭제에 성공하셨습니다'})
     else:
         return jsonify({'result': 'fail', 'msg': '삭제권한이 없습니다'})
-
-
-# 메인페이지에서 게시물 클릭 시 상세페이지로 이동
-@app.get('/api/postings-detail/<int:post_id>')
-def post_info(post_id):
-    page_info = db.postings.find_one({'post_id': post_id}, {'_id': False})
-    print(page_info)
-    return render_template('detail.html', page=page_info)
 
 
 # 회원 유효성 검사
