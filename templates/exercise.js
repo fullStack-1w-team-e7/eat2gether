89f29fcc9d671f4636264f456f3bd9dba135185d
TibbetRabbit <dayoungsieun@gmail.com>
2022-11-17 11:46:50 +0900

    no message

---
+++ templates/exercise.js
@@ -0,0 +1,17 @@
+const response = {'id': 'didi', 'age':27}
+
+
+    'id': 'didi', 'id': 'wori', 'id': 'bibi', 'id': 'haha', 'id': 'lili', 'age': 27}
+
+let ids = response['id']
+
+
+console.log(ids)
+
+
+/* for (let i=0; i<response.length; i++) {
+    console.log(response[i])
+
+}
+
+*/
